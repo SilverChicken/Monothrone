13640fbe1d4fc9de8a4ba195df48345c358e91d0
Quentin Tang <quentin.tang@softwareengineering.it>
2021-03-03 05:53:48 +0100

    Added GameMode

--- Gamemode.cpp
+++ Gamemode.cpp
@@ -1,1 +1,28 @@
 #include "Gamemode.h"
+
+Gamemode::~Gamemode()
+{
+}
+
+Gamemode & Gamemode::getInstance()
+{
+	// TODO: insert return statement here
+}
+
+Unit * Gamemode::getThrone(int)
+{
+	return nullptr;
+}
+
+Player * Gamemode::getPlayer(int)
+{
+	return nullptr;
+}
+
+void Gamemode::addThrone(int, Unit *)
+{
+}
+
+void Gamemode::addPlayer(int, Player *)
+{
+}
